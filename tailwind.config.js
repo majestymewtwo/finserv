ef4f7537f0dffd8ee22224201aa92d157a5a22f5
majestymewtwo <98326903+majestymewtwo@users.noreply.github.com>
2022-05-29 13:46:35 +0530

    First Commit

---
+++ tailwind.config.js
@@ -0,0 +1,14 @@
+module.exports = {
+  content: ["*"],
+  theme: {
+    extend: {
+      fontFamily: {
+        Rampart: ["Rampart One", "cursive"],
+        Ubuntu: ["Ubuntu Condensed", "sans-serif"],
+        Arima: ["Arima Madurai", "cursive"],
+
+      },
+    },
+  },
+  plugins: [],
+}
